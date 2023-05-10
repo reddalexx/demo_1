64773bebc175a4009fd2dee6032b73171b307918
alex <reddalexx@gmail.com>
2023-05-10 15:43:47 +0300

    minor updates

--- .deploy/docker/build/main/build_image.sh
+++ .deploy/docker/build/main/build_image.sh
@@ -4,11 +4,10 @@ set -a
 
 source ../../.env
 
-rm -f start.sh
-
 envsubst < "start-template.sh" > "start.sh"
 
 pushd ../../../../ || return
-#docker volume rm demo_django_app || true
 docker build -t demo_main_app -f ./.deploy/docker/build/main/Dockerfile .
 popd || return
+
+rm -f start.sh
