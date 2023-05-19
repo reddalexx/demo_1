ff551f29241e8cc5a5e7375ef35d9c3e2ecceca3
alex <reddalexx@gmail.com>
2023-05-19 19:44:00 +0300

    updated unit tests

--- apps/geo/tests.py
+++ apps/geo/tests.py
@@ -1,11 +1,12 @@
-from django.db.models import F, Count
+from django.conf import settings
+from django.db.models import F, Count, Q
 from django.template.exceptions import TemplateDoesNotExist
 from django.test import TestCase
 from django.urls import reverse
 
 from apps.geo.models import Country, City
 from apps.geo.serializers import CountrySerializer
-from apps.geo.views import CountryListView, ChartsView, CityListView
+from apps.geo.views import CountryListView, ChartsView, CityListView, FilterableCityViewSet
 
 
 class TestCountry(TestCase):
@@ -38,11 +39,79 @@ class TestCountry(TestCase):
         self.assertEqual(Country.objects.get(pk=1).name, new_name)
 
     def test_list_api(self):
-        url = reverse('geo:drf-api-country-list')
+        url = reverse('geo:drf-api-country-list') + '?format=datatables'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), Country.objects.count())
-        self.assertDictEqual(response.data[0], CountrySerializer(Country.objects.get(pk=1)).data)
+        self.assertEqual(response.data['recordsTotal'], Country.objects.count()),
+        self.assertEqual(len(response.data['data']), min(Country.objects.count(), settings.REST_FRAMEWORK['PAGE_SIZE'])),
+        self.assertDictEqual(response.data['data'][0], CountrySerializer(Country.objects.get(pk=1)).data)
+
+    def test_list_api_datatables_filtering(self):
+        search_str = 'Mex'
+        columns = ['id', 'name', 'iso3', 'area', 'population']
+        # columns = ['id', 'name', 'country_name', 'country_iso3', 'lat', 'lng', 'population']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][searchable]=true' for n, col in enumerate(columns)]),
+            f'search[value]={search_str}&search[regex]=false'])
+        url = reverse('geo:drf-api-country-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data['recordsTotal'],
+            Country.objects.count())
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            Country.objects.filter(name__icontains=search_str).count())
+        self.assertEqual(
+            len(response.data['data']),
+            Country.objects.filter(name__icontains=search_str).count())
+
+    def test_list_api_datatables_ordering(self):
+        order_col_name = 'iso3'
+        direction = 'desc'
+        columns = ['id', 'name', 'iso3', 'area', 'population']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][orderable]=true' for n, col in enumerate(columns)]),
+            f'order[0][column]={columns.index(order_col_name)}&order[0][dir]={direction}'])
+        url = reverse('geo:drf-api-country-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data['recordsTotal'],
+            Country.objects.count())
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            Country.objects.count())
+        self.assertEqual(
+            response.data['data'][0]['iso3'],
+            getattr(Country.objects.order_by(f'{"-" if direction == "desc" else ""}{order_col_name}').first(), order_col_name))
+
+    def test_list_api_datatables_pagination(self):
+        order_col_name = 'iso3'
+        direction = 'desc'
+        page_offset = 10
+        page_size = 10
+        columns = ['id', 'name', 'iso3', 'area', 'population']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][orderable]=true' for n, col in enumerate(columns)]),
+            f'order[0][column]={columns.index(order_col_name)}&order[0][dir]={direction}',
+            f'start={page_offset}&length={page_size}'
+        ])
+        url = reverse('geo:drf-api-country-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data['recordsTotal'],
+            Country.objects.count())
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            Country.objects.count())
+        self.assertEqual(
+            response.data['data'][0][order_col_name],
+            getattr(Country.objects.order_by(f'{"-" if direction == "desc" else ""}{order_col_name}')[page_offset:page_offset+page_size].first(), order_col_name))
 
     def test_detail_api(self):
         country = Country.objects.get(id=1)
@@ -124,12 +193,122 @@ class CityTest(TestCase):
                              target_status_code=200, fetch_redirect_response=True)
         self.assertEqual(City.objects.get(pk=1).name, new_name)
 
-    def test_list_api(self):
-        url = reverse('geo:drf-api-city-list')
+    def test_list_api_datatables(self):
+        url = reverse('geo:drf-api-city-list') + '?format=datatables'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), City.objects.count())
-        self.assertEqual(response.data[0]['name'], City.objects.get(pk=1).name)
+        self.assertEqual(len(response.data['data']), min(City.objects.count(), settings.REST_FRAMEWORK['PAGE_SIZE'])),
+        self.assertEqual(response.data['data'][0]['name'], City.objects.get(pk=1).name)
+
+    def test_list_api_datatables_filtering(self):
+        search_str = 'Mex'
+        columns = ['id', 'name', 'country_name', 'country_iso3', 'lat', 'lng', 'population']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][searchable]=true' for n, col in enumerate(columns)]),
+            f'search[value]={search_str}&search[regex]=false'])
+        url = reverse('geo:drf-api-city-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data['recordsTotal'],
+            City.objects.count())
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            City.objects.filter(Q(name__icontains=search_str) |
+                                Q(country__name__icontains=search_str) |
+                                Q(country__iso3__icontains=search_str)).count())
+        self.assertEqual(
+            len(response.data['data']),
+            response.data['recordsFiltered'])
+
+    def test_list_api_datatables_ordering(self):
+        order_col_name = 'name'
+        direction = 'desc'
+        columns = ['id', 'name', 'country_name', 'country_iso3', 'lat', 'lng', 'population']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][orderable]=true' for n, col in enumerate(columns)]),
+            f'order[0][column]={columns.index(order_col_name)}&order[0][dir]={direction}'])
+        url = reverse('geo:drf-api-city-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data['recordsTotal'],
+            City.objects.count())
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            City.objects.count())
+        self.assertEqual(
+            response.data['data'][0][order_col_name],
+            getattr(City.objects.order_by(f'{"-" if direction == "desc" else ""}{order_col_name}').first(), order_col_name))
+
+    def test_list_api_datatables_pagination(self):
+        order_col_name = 'name'
+        direction = 'desc'
+        page_offset = 10
+        page_size = 10
+        columns = ['id', 'name', 'country_name', 'country_iso3', 'lat', 'lng', 'population']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][orderable]=true' for n, col in enumerate(columns)]),
+            f'order[0][column]={columns.index(order_col_name)}&order[0][dir]={direction}',
+            f'start={page_offset}&length={page_size}'
+        ])
+        url = reverse('geo:drf-api-city-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data['recordsTotal'],
+            City.objects.count())
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            City.objects.count())
+        ordering = f'{"-" if direction == "desc" else ""}{order_col_name}'
+        self.assertEqual(
+            response.data['data'][0][order_col_name],
+            getattr(City.objects.order_by(ordering)[page_offset:page_offset+page_size].first(), order_col_name))
+
+    # test list view with default DRF filter/sort
+    def test_list_api_default_search(self):
+        url = reverse('geo:drf-api-city-search-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']),
+                         min(City.objects.count(), FilterableCityViewSet.pagination_class.page_size)),
+        self.assertEqual(response.data['results'][0]['name'], City.objects.get(pk=1).name)
+
+    def test_list_api_default_search_filtering(self):
+        search_str = 'AF'
+        url = reverse('geo:drf-api-city-search-list') + f'?search={search_str}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        expected = City.objects.filter(Q(name__icontains=search_str) |
+                                       Q(country__name__icontains=search_str) |
+                                       Q(country__iso3__icontains=search_str))
+        self.assertEqual(response.data['count'], expected.count())
+        self.assertIn(response.data['results'][0]['id'], expected.values_list('id', flat=True))
+
+    def test_list_api_django_filters_filtering(self):
+        search_str = 'Paris'
+        url = reverse('geo:drf-api-city-search-list') + f'?name={search_str}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        expected = City.objects.filter(name__exact=search_str)
+        self.assertEqual(response.data['count'], expected.count())
+        self.assertIn(response.data['results'][0]['id'], expected.values_list('id', flat=True))
+
+    def test_list_api_default_search_ordering(self):
+        order_col_name = 'name'
+        direction = 'desc'
+        ordering = f'{"-" if direction == "desc" else ""}{order_col_name}'
+        url = reverse('geo:drf-api-city-search-list') + f'?ordering={ordering}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], City.objects.count())
+        self.assertEqual(
+            response.data['results'][0][order_col_name],
+            getattr(City.objects.order_by(ordering).first(), order_col_name))
 
     def test_detail_api(self):
         city = City.objects.get(id=1)
--- apps/hotels/tests.py
+++ apps/hotels/tests.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.db.models import Q
 from django.test import TestCase
 from django.urls import reverse
 
@@ -33,12 +35,71 @@ class TestSelectedHotelsAPI(TestCase):
         self.hotel_1 = mixer.blend(Hotel, name='Hotel One', city=City.objects.first())
         self.hotel_2 = mixer.blend(Hotel, name='Hotel Two', city=City.objects.last())
 
-    def test_list_api(self):
-        url = reverse('hotels:drf-api-hotel-list')
+    def test_list_api_datatables(self):
+        url = reverse('hotels:drf-api-hotel-list') + '?format=datatables'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), Hotel.objects.count())
-        self.assertEqual(response.data[0]['name'], Hotel.objects.get(pk=response.data[0]['id']).name)
+        self.assertEqual(len(response.data['data']), min(Hotel.objects.count(), settings.REST_FRAMEWORK['PAGE_SIZE'])),
+        self.assertEqual(response.data['data'][0]['name'], Hotel.objects.get(pk=response.data['data'][0]['id']).name)
+
+    def test_list_api_datatables_filtering(self):
+        search_str = 'Two'
+        columns = ['id', 'name', 'city_name', 'country_name', 'description']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][searchable]=true' for n, col in enumerate(columns)]),
+            f'search[value]={search_str}&search[regex]=false'])
+        url = reverse('hotels:drf-api-hotel-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['recordsTotal'], Hotel.objects.count())
+        self.assertEqual(len(response.data['data']), response.data['recordsFiltered'])
+        self.assertEqual(
+            response.data['recordsFiltered'],
+            Hotel.objects.filter(Q(name__icontains=search_str) |
+                                 Q(city__name__icontains=search_str) |
+                                 Q(city__country__name__icontains=search_str)).count())
+
+    def test_list_api_datatables_ordering(self):
+        order_col_name = 'name'
+        direction = 'desc'
+        columns = ['id', 'name', 'city_name', 'country_name', 'description']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][orderable]=true' for n, col in enumerate(columns)]),
+            f'order[0][column]={columns.index(order_col_name)}&order[0][dir]={direction}'])
+        url = reverse('hotels:drf-api-hotel-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        expected_total_hotels = Hotel.objects.count()
+        self.assertEqual(response.data['recordsTotal'], expected_total_hotels)
+        self.assertEqual(response.data['recordsFiltered'], expected_total_hotels)
+        self.assertEqual(
+            response.data['data'][0][order_col_name],
+            getattr(Hotel.objects.order_by(f'{"-" if direction == "desc" else ""}{order_col_name}').first(), order_col_name))
+
+    def test_list_api_datatables_pagination(self):
+        order_col_name = 'name'
+        direction = 'desc'
+        page_offset = 1
+        page_size = 1
+        columns = ['id', 'name', 'city_name', 'country_name', 'description']
+        query_str = '&'.join([
+            '?format=datatables',
+            '&'.join([f'columns[{n}][data]={col}&columns[{n}][orderable]=true' for n, col in enumerate(columns)]),
+            f'order[0][column]={columns.index(order_col_name)}&order[0][dir]={direction}',
+            f'start={page_offset}&length={page_size}'
+        ])
+        url = reverse('hotels:drf-api-hotel-list') + query_str
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        expected_total_hotels = Hotel.objects.count()
+        self.assertEqual(response.data['recordsTotal'], expected_total_hotels)
+        self.assertEqual(response.data['recordsFiltered'], expected_total_hotels)
+        ordering = f'{"-" if direction == "desc" else ""}{order_col_name}'
+        self.assertEqual(
+            response.data['data'][0][order_col_name],
+            getattr(Hotel.objects.order_by(ordering)[page_offset:page_offset+page_size].first(), order_col_name))
 
     def test_detail_api(self):
         hotel = Hotel.objects.first()
